a4aa6fa3fc1f16f192d2189ac0d9c2d0e7d541fb
raygdev <101662915+raygdev@users.noreply.github.com>
2022-08-13 19:51:03 -0400

    Remove background image property"
    -moved to show page
    -variable was not defined in file

--- src/utils/styles.js
+++ src/utils/styles.js
@@ -2,7 +2,6 @@ let IMG_URL = "https://image.tmdb.org/t/p/w500";
 
 const styles = {
     backgroundColor: "rgba(0,0,0,0.6)",
-    backgroundImage: `url(${IMG_URL}${showDetails?.backdrop_path})`,
     backgroundBlendMode: "multiply",
     backgroundRepeat: "no-repeat",
     backgroundSize: "cover",
