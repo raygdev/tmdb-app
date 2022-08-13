997ab0bb0d9cd5944259f1104664881a47e17781
raygdev <101662915+raygdev@users.noreply.github.com>
2022-08-13 19:34:56 -0400

    Add styles to file
    separation of concerns

---
+++ src/utils/styles.js
@@ -0,0 +1,10 @@
+const styles = {
+    backgroundColor: "rgba(0,0,0,0.6)",
+    backgroundImage: `url(${IMG_URL}${showDetails.backdrop_path})`,
+    backgroundBlendMode: "multiply",
+    backgroundRepeat: "no-repeat",
+    backgroundSize: "cover",
+    color: "white",
+  };
+
+  export default styles
