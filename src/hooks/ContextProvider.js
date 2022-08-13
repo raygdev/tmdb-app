e35241e86432564b409c29f4d4845529f9f2993c
raygdev <101662915+raygdev@users.noreply.github.com>
2022-08-13 18:32:31 -0400

    Remove unused variables from Context
    -Remove unused or non-existent variabls from value prop of Context
    -Remove unused state
    -unused state in context is provided in own custom hook.

--- src/hooks/ContextProvider.js
+++ src/hooks/ContextProvider.js
@@ -3,9 +3,7 @@ import { createContext, useState } from "react";
 const Context = createContext();
 
 const ContextProvider = ({ children }) => {
-  const [isLoading, setIsLoading] = useState(false);
   const [motionPicture, setMotionPicture] = useState("movie");
-  const loader = <div className="loader"></div>;
 
   function toggleMotionPicture() {
     setMotionPicture((prevMotionPicture) =>
@@ -16,12 +14,8 @@ const ContextProvider = ({ children }) => {
   return (
     <Context.Provider
       value={{
-        isLoading,
-        setIsLoading,
-        loader,
         motionPicture,
         toggleMotionPicture,
-        URL,
       }}
     >
       {children}
