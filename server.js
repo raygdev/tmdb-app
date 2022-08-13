30b81c7d76f216b633da8a5dd7fbcba13e30c4e2
raygdev <101662915+raygdev@users.noreply.github.com>
2022-08-13 14:21:30 -0400

    Add motion picture routes to app

--- server.js
+++ server.js
@@ -2,7 +2,9 @@ const express = require("express");
 require("dotenv").config();
 const app = express();
 const cors = require("cors");
+
 const motionPicture = require('./controllers/controllers')
+const motionPictureRouter = require('./router/routes')
 
 
 const port = process.env.MY_PORT || 5000;
@@ -12,6 +14,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(motionPicture.logger);
 
+app.use(motionPictureRouter)
 
 app.listen(port, () => {
   console.log(`App is listening on port ${port}`);
