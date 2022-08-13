b0de21e8da25293c4037aecf71afe81393cb473f
raygdev <101662915+raygdev@users.noreply.github.com>
2022-08-13 19:48:37 -0400

    Refactor show pages
    -removed styles object and stored in utils
    -reformatted file
    -imported styles obeject from utils

--- src/pages/Shows.js
+++ src/pages/Shows.js
@@ -3,66 +3,57 @@ import { useParams, Link } from "react-router-dom";
 import { ImageLinkSlider } from "../components/ImageLinkSlider";
 import { useLoading } from "../hooks/useLoading";
 import { displaySimilarTitles } from "../utils/utils";
+import styles from "../utils/styles";
 
 let IMG_URL = "https://image.tmdb.org/t/p/w500";
 
 const Shows = (props) => {
   const [showDetails, setShowDetails] = useState("");
-  const {
-    isLoading,
-    setIsLoading,
-    loader
-  } = useLoading()
+  const { isLoading, setIsLoading, loader } = useLoading();
   const { show_id } = useParams();
 
   useEffect(() => {
-    setIsLoading(true)
+    setIsLoading(true);
     fetch(`/api/tvshow/${show_id}`, { method: "POST" })
       .then((res) => res.json())
       .then((data) => {
-        setShowDetails(data)
-        setIsLoading(false)
+        setShowDetails(data);
+        setIsLoading(false);
       })
       .catch((e) => console.log(e));
   }, [show_id]);
 
-  let genres =showDetails?.genres?.map((genre, i) => {
-        return (
-          <p className={`genres${i} genre`} key={genre.id}>
-            {
-              <Link to={`/genres/${genre.id}/${genre.name}/tv`}>
-                {genre.name}
-              </Link>
-            }
-          </p>
-        );
-      });
+  let genres = showDetails?.genres?.map((genre, i) => {
+    return (
+      <p className={`genres${i} genre`} key={genre.id}>
+        {<Link to={`/genres/${genre.id}/${genre.name}/tv`}>{genre.name}</Link>}
+      </p>
+    );
+  });
 
   const { similar, credits } = showDetails;
 
-  let similarTitle =  displaySimilarTitles(similar?.results, "/shows/selected");
+  let similarTitle = displaySimilarTitles(similar?.results, "/shows/selected");
   let cast = displaySimilarTitles(credits?.cast, "/people/selected");
   const setImagesFirst = credits?.crew.reduce((prev, curre) => {
-        if (!curre.profile_path) {
-          prev.push(curre);
-        } else prev.unshift(curre);
-        return prev;
-      }, []);
-  let crew = displaySimilarTitles(setImagesFirst, "/people/selected")
-    
+    if (!curre.profile_path) {
+      prev.push(curre);
+    } else prev.unshift(curre);
+    return prev;
+  }, []);
+  let crew = displaySimilarTitles(setImagesFirst, "/people/selected");
 
-  const styles = {
-    backgroundColor: "rgba(0,0,0,0.6)",
-    backgroundImage: `url(${IMG_URL}${showDetails.backdrop_path})`,
-    backgroundBlendMode: "multiply",
-    backgroundRepeat: "no-repeat",
-    backgroundSize: "cover",
-    color: "white",
-  };
-
-  return (
-    isLoading ? (loader) : (<div>
-      <div className="movie-info-container" style={styles}>
+  return isLoading ? (
+    loader
+  ) : (
+    <div>
+      <div
+        className="movie-info-container"
+        style={{
+          ...styles,
+          backgroundImage: `url(${IMG_URL}${showDetails?.backdrop_path})`,
+        }}
+      >
         <div className="movie-info-content">
           <div className="img-col">
             <img src={`${IMG_URL}${showDetails.poster_path}`} alt="" />
@@ -83,9 +74,9 @@ const Shows = (props) => {
           name={`Titles Related to ${showDetails.name}`}
         />
         <ImageLinkSlider images={cast} name="Cast" />
-        { crew  && <ImageLinkSlider images={crew} name="Crew" />}
+        {crew && <ImageLinkSlider images={crew} name="Crew" />}
       </div>
-    </div>)
+    </div>
   );
 };
 
