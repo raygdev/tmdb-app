2cff6ba9cfe9e46066c6cb2c81310d47f7fe38fc
raygdev <101662915+raygdev@users.noreply.github.com>
2022-08-13 18:21:10 -0400

    Remove unused variable

--- src/index.js
+++ src/index.js
@@ -2,8 +2,7 @@ import React from "react";
 // import ReactDOM from "react-dom";
 import "./css/index.css"
 
-import App from "./App";
-import reportWebVitals from "./reportWebVitals";
+import App from "./App"
 import { BrowserRouter as Router } from "react-router-dom";
 import { ContextProvider } from "./hooks/ContextProvider";
 import ScrollToTop from "./components/ScrollToTop";
