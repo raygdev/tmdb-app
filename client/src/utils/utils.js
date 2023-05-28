0ce9262a9fbef2356b294e0c9f69a490b888feea
raygdev <101662915+raygdev@users.noreply.github.com>
2023-05-27 20:12:58 -0400

    Clean and reformat
    cleans up the displaySimilarTitles util
    and reformat the file
    
    TODO extract jsx into it's own component

--- client/src/utils/utils.js
+++ client/src/utils/utils.js
@@ -1,61 +1,63 @@
-import { Link } from "react-router-dom";
-import React from "react";
+import { Link } from "react-router-dom"
+import React from "react"
 
-const imageSource = "https://image.tmdb.org/t/p/w200";
-const noPhotoUrl ="https://images.freeimages.com/clg/images/41/413691/none-icon-available-no-unavailable_f.jpg"
+const imageSource = "https://image.tmdb.org/t/p/w200"
+const noPhotoUrl =
+  "https://images.freeimages.com/clg/images/41/413691/none-icon-available-no-unavailable_f.jpg"
 
-function displaySimilarTitles(titles, path, toggle) {
-  let hash = {};
-  let images = titles?.filter(
-      (title) => title.poster_path || title.profile_path || !title.profile_path
+function displaySimilarTitles(titles, path) {
+  let hash = {}
+  let images = titles
+    ?.filter(
+      title => title.poster_path || title.profile_path || !title.profile_path
     )
-    .map((title, i) => {
+    .map(title => {
+      let imagePath = title.poster_path
+        ? `${imageSource}${title.poster_path}`
+        : title.profile_path
+        ? `${imageSource}${title.profile_path}`
+        : noPhotoUrl
       if (!hash[title.id]) {
-        hash[title.id] = title.id;
+        hash[title.id] = title.id
         return (
-          <div className={`movie-container movie-container${i}`} key={title.id}>
-            <Link to={`${path}/${title.id}`} onClick={toggle}>
+          <div className={`movie-container`} key={title.id}>
+            <Link to={`${path}/${title.id}`}>
               <img
                 loading="lazy"
-                src={
-                  title.poster_path
-                    ? `${imageSource}${title.poster_path}`
-                    : title.profile_path
-                    ? `${imageSource}${title.profile_path}`
-                    : noPhotoUrl
-                }
+                src={imagePath}
                 alt={title.title || title.name}
               />
             </Link>
-            <h3 data-tool-tip={`${title.title || title.name}`} className="name-truncate" style={{ marginTop: "0", marginBottom: "0" }}>
+            <h3
+              data-tool-tip={`${title.title || title.name}`}
+              className="name-truncate"
+            >
               {title.name || title.title}
             </h3>
           </div>
-        );
+        )
       }
-    });
-  return images;
+    })
+  return images
 }
 
-function setImageFirst(people){
+function setImageFirst(people) {
   let image = []
   let non_image = []
   people?.forEach(person => {
-      if(!person.profile_path){
-        non_image.push(person)
-      }else{
-        image.push(person)
-      }
+    if (!person.profile_path) {
+      non_image.push(person)
+    } else {
+      image.push(person)
+    }
   })
   return image.concat(non_image)
 }
 
-
-
-function truncate(string){
+function truncate(string) {
   return string?.length > 175
-  ? string.slice(0, 175).trim().concat(`...`)
-  : string;
+    ? string.slice(0, 175).trim().concat(`...`)
+    : string
 }
 
-export { displaySimilarTitles, setImageFirst, truncate };
+export { displaySimilarTitles, setImageFirst, truncate }
