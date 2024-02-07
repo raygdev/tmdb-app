e91d4ff654f84a7798ba242cbc56aa48a7c9a6df
raygdev <101662915+raygdev@users.noreply.github.com>
2024-02-06 22:28:04 -0500

    Change server request

--- client/src/utils/apiUrl.js
+++ client/src/utils/apiUrl.js
@@ -1,8 +1,9 @@
 let API_URL;
 if(process.env.NODE_ENV === "production"){
-    API_URL = "https://tmdb-api-rwj0.onrender.com"
+    API_URL = 'https://eager-polo-shirt-crow.cyclic.app'
 } else {
     API_URL = "http://localhost:8080"
 }
 
 export { API_URL }
+// API_URL = "https://tmdb-api-rwj0.onrender.com"
