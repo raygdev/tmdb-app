6c3f97887ad646ccf08ccbd043fa46a3b543660d
raygdev <101662915+raygdev@users.noreply.github.com>
2024-05-05 19:39:22 -0400

    Add default value for hook

--- client/src/hooks/useLoading/useLoading.js
+++ client/src/hooks/useLoading/useLoading.js
@@ -1,8 +1,8 @@
 import React, {useState} from 'react'
 import loadingStyles from "./useLoading.module.css"
 
-function useLoading(){
-    const [isLoading, setIsLoading]  = useState(false)
+function useLoading(value = false){
+    const [isLoading, setIsLoading]  = useState(value)
     const loader = <div className={loadingStyles.loader}></div>
 
     return {isLoading, setIsLoading, loader}
