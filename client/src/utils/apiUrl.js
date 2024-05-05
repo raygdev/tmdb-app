e94e190f88f14b098396a903fbb7a6bce136a277
raygdev <101662915+raygdev@users.noreply.github.com>
2024-05-05 19:12:52 -0400

    Change port for local host

--- client/src/utils/apiUrl.js
+++ client/src/utils/apiUrl.js
@@ -2,7 +2,7 @@ let API_URL;
 if(process.env.NODE_ENV === "production"){
     API_URL = 'https://eager-polo-shirt-crow.cyclic.app'
 } else {
-    API_URL = "http://localhost:8080"
+    API_URL = "http://localhost:5000"
 }
 
 export { API_URL }
