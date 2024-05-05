9b1fda305c977ea2e12a3ad3348ead17e1d2d045
raygdev <101662915+raygdev@users.noreply.github.com>
2024-05-05 19:13:37 -0400

    Use hook from extracted logic
    remove unused imports and clean up conditional

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,35 +1,20 @@
-import React, { useState, useEffect, useContext } from "react";
 import { ImageLinkSlider } from "../components/imageLinkSlider/ImageLinkSlider";
 import { SpanBtn } from "../components/spanBtn/SpanBtn";
-import { Context } from "../hooks/ContextProvider";
-import { useLoading } from "../hooks/useLoading/useLoading";
 import { displaySimilarTitles } from "../utils/utils";
-import { API_URL } from "../utils/apiUrl";
+import { useBaseMotionPicture } from "../hooks/useBaseMotionPicture";
+
 
 
 const Home = (props) => {
-  const [movies, setMovies] = useState([]);
   const {
+    movies,
     isLoading,
-    setIsLoading,
-    loader
-  } = useLoading()
-
-  const { selectMotionPicture, motionPicture } = useContext(Context);
+    loader,
+    motionPicture,
+    selectMotionPicture 
+  } = useBaseMotionPicture()
+  
   document.title = "Home"
- 
-  useEffect(() => {
-    setIsLoading(true)
-    fetch(`${API_URL}/api/motionpicture?motionPicture=${motionPicture}`)
-      .then((res) => res.json())
-      .then((data) => {
-          setMovies( data.results);
-          setIsLoading(false)
-          document.querySelector("meta[name='description']").setAttribute("content", "Search movies and tv shows and create a watchlist for shows to watch")  
-      })
-      .catch((e) => console.log(e));
-      
-  }, [motionPicture]);
 
   const isSelected = motionPicture === "movie"
   const motionPicturePath = motionPicture === "movie" ? "movie" : "shows";
@@ -39,28 +24,30 @@ const Home = (props) => {
     `/${motionPicturePath}/selected`
   );
 
+  if(isLoading) return loader
+
   return (
-    isLoading ? (loader) :
-     (<div className="home_container">
-        <div className="toggle_btn_container">
-          <SpanBtn
-            selectMotionPicture={selectMotionPicture}
-            isSelected={isSelected}
-            name={"Movies"}
-            selectedValue={"movie"}
-          />
-          <span> -or- </span>
-          <SpanBtn
-            selectMotionPicture={selectMotionPicture}
-            isSelected={!isSelected}
-            name={"Shows"}
-            selectedValue={"tv"}
-          />
-          
-        </div>
-        <ImageLinkSlider images={trendingMovies} name="Trending" />
+    
+    <div className="home_container">
+      <div className="toggle_btn_container">
+        <SpanBtn
+          selectMotionPicture={selectMotionPicture}
+          isSelected={isSelected}
+          name={"Movies"}
+          selectedValue={"movie"}
+        />
+        <span> -or- </span>
+        <SpanBtn
+          selectMotionPicture={selectMotionPicture}
+          isSelected={!isSelected}
+          name={"Shows"}
+          selectedValue={"tv"}
+        />
+        
       </div>
-      )
-  );
+      <ImageLinkSlider images={trendingMovies} name="Trending" />
+    </div>
+  )
+  
 };
 export default Home;
