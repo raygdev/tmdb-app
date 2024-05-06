b5b61e56261ba20a4016d36236fe5091094a6e9e
raygdev <101662915+raygdev@users.noreply.github.com>
2024-05-06 11:25:04 -0400

    Move api calls to services

---
+++ client/src/services/motionPictureService.js
@@ -0,0 +1,16 @@
+import { API_URL } from "../utils/apiUrl";
+
+export async function getMotionPictures(motionPicture, signal) {
+    const res = await fetch(`${API_URL}/api/motionpicture?motionPicture=${motionPicture}`, { signal })
+    const data = await res.json()
+    return data.results
+}
+
+export async function getMotionPicturesFromGenre({ genre_id, page, motion_picture }, signal) {
+    const res = await fetch(
+        `${API_URL}/api/genres/?with_genres=${genre_id}&page=${page}&motion_picture=${motion_picture}`,
+        { signal }
+      )
+    const data = await res.json()
+    return data
+}
