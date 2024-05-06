e14f5843e0fbafa4f547afb13eedd3d1a27383d6
raygdev <101662915+raygdev@users.noreply.github.com>
2024-05-06 12:08:48 -0400

    Add get by id for show and movie

--- client/src/services/motionPictureService.js
+++ client/src/services/motionPictureService.js
@@ -14,3 +14,15 @@ export async function getMotionPicturesFromGenre({ genre_id, page, motion_pictur
     const data = await res.json()
     return data
 }
+
+export async function getMovieById(id, signal) {
+    const res = await fetch(`${API_URL}/api/movie/${id}`, { signal })
+    const data = await res.json()
+    return data
+}
+
+export async function getShowById(id, signal) {
+    const res = await fetch(`${API_URL}/api/tvshow/${id}`, { signal })
+    const data = await res.json()
+    return data
+}
