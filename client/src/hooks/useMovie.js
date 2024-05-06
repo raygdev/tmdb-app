5e2da560bd9eb8c09991d52343b054c2037574f7
raygdev <101662915+raygdev@users.noreply.github.com>
2024-05-06 12:09:39 -0400

    Add getById and refactor

--- client/src/hooks/useMovie.js
+++ client/src/hooks/useMovie.js
@@ -1,23 +1,29 @@
 import { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
 import { useLoading } from "./useLoading/useLoading";
-import { API_URL } from "../utils/apiUrl";
+import { getMovieById } from "../services/motionPictureService";
 
 export function useMovie() {
-    const [movieDetails, setMovieDetails] = useState([]);
-    const { isLoading, setIsLoading, loader } = useLoading();
-    const { id } = useParams()
-    useEffect(() => {
-        setIsLoading(true);
-        fetch(`${API_URL}/api/movie/${id}`)
-          .then((res) => res.json())
-          .then((data) => {
-            setMovieDetails(data);
-            setIsLoading(false);
-            document.title =  data.title
-            document.querySelector("meta[name='description']").setAttribute("content", `${data.title} - ${data.overview}`)
-          });
-      }, [id]);
-    
-    return { movieDetails, isLoading, loader }
+  const [movieDetails, setMovieDetails] = useState([]);
+  const { isLoading, setIsLoading, loader } = useLoading();
+  const { id } = useParams();
+  useEffect(() => {
+    setIsLoading(true);
+    const controller = new AbortController();
+    const signal = controller.signal;
+    getMovieById(id, signal)
+      .then((data) => {
+        setMovieDetails(data);
+        document.title = data.title;
+        document
+          .querySelector("meta[name='description']")
+          .setAttribute("content", `${data.title} - ${data.overview}`);
+      })
+      .catch((e) => console.log(e))
+      .finally(() => setIsLoading(false));
+
+    return () => controller.abort();
+  }, [id]);
+
+  return { movieDetails, isLoading, loader };
 }
