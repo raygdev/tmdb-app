3615f221b9061fa0106538855257996c1ed89272
raygdev <101662915+raygdev@users.noreply.github.com>
2024-05-06 12:11:28 -0400

    Add getById and refactor

--- client/src/hooks/useShow.js
+++ client/src/hooks/useShow.js
@@ -1,25 +1,29 @@
 import { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
 import { useLoading } from "./useLoading/useLoading";
-import { API_URL } from "../utils/apiUrl";
+import { getShowById } from "../services/motionPictureService";
 
 export function useShow() {
-    const [showDetails, setShowDetails] = useState("");
-    const { isLoading, setIsLoading, loader } = useLoading(true);
-    const { show_id } = useParams();
-  
-    useEffect(() => {
-      if(!isLoading) setIsLoading(true)
-      fetch(`${API_URL}/api/tvshow/${show_id}`)
-        .then((res) => res.json())
-        .then((data) => {
-          setShowDetails(data);
-          document.title = data.name
-          document.querySelector("meta[name='description']").setAttribute("content", `${data.name} - ${data.overview}`)
-        })
-        .catch((e) => console.log(e))
-        .finally(() => setIsLoading(false))
-    }, [show_id]);
+  const [showDetails, setShowDetails] = useState("");
+  const { isLoading, setIsLoading, loader } = useLoading(true);
+  const { show_id } = useParams();
 
-    return { showDetails, isLoading, loader }
+  useEffect(() => {
+    if (!isLoading) setIsLoading(true);
+    const controller = new AbortController();
+    const signal = controller.signal;
+    getShowById(show_id, signal)
+      .then((data) => {
+        setShowDetails(data);
+        document.title = data.name;
+        document
+          .querySelector("meta[name='description']")
+          .setAttribute("content", `${data.name} - ${data.overview}`);
+      })
+      .catch((e) => console.log(e))
+      .finally(() => setIsLoading(false));
+    return () => controller.abort("page change");
+  }, [show_id]);
+
+  return { showDetails, isLoading, loader };
 }
