9a113adc7781b703028a57bd8d0020f8b21ab3ff
raygdev <101662915+raygdev@users.noreply.github.com>
2024-08-18 21:32:32 -0400

    Add api directory for vercel

---
+++ server/api/index.js
@@ -0,0 +1,29 @@
+const express = require("express");
+require("dotenv").config();
+const app = express();
+const cors = require("cors");
+
+const motionPicture = require('../controllers/controllers')
+const motionPictureRouter = require('../router/routes')
+
+
+const port = process.env.MY_PORT || 5000;
+
+let whiteList = [
+  "https://tmdb-app-8pkp.vercel.app",
+  "https://tmdb-app-8pkp-raygdev.vercel.app",
+  "https://tmdb-app-apkp-git-main-raygdev.vercel.app",
+  // "http://localhost:3000",
+]
+
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(motionPicture.logger);
+
+app.use(motionPictureRouter)
+
+app.listen(port, "0.0.0.0", () => {
+  console.log(`App is listening on port ${port}`);
+});
+
