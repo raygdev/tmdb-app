f16c25a7e706c0a5d2fe4646ee3819a3a6d22ac0
raygdev <101662915+raygdev@users.noreply.github.com>
2024-08-18 21:43:17 -0400

    Add back Allow-Origin

--- server/api/index.js
+++ server/api/index.js
@@ -16,7 +16,15 @@ let whiteList = [
   // "http://localhost:3000",
 ]
 
-app.use(cors());
+app.use(cors({
+  origin: function(origin,callback) {
+    if(whiteList.indexOf(origin) !== -1){
+      callback(null, true)
+    } else {
+      callback( new Error("Blocked by cors"))
+    }
+  }
+}));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(motionPicture.logger);
